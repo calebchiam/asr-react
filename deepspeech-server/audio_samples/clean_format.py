463c1ab1ec4c9d5fe9fe2068eed6ccea068a0a55
calebchiam <calebchiam@gmail.com>
2019-08-26 01:37:20 +0800

    cleaning audio_samples

--- deepspeech-server/audio_samples/clean_format.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("formats.txt", 'r') as f:
-    formats = {"valid": []}
-    for line in f:
-        args = [x for x in line.split(" ") if x.strip() != ""]
-        formats["valid"].append(args[1])
-    with open("formats.json", 'w') as g:
-        json.dump(formats, g)
